79abea2fdb2c5e3f2d961386c0e7c0eca4a5cbce
rockxxxxxx <78143717+rockxxxxxx@users.noreply.github.com>
2022-10-08 13:01:22 +0530

    added search functionality using filter and includes methods of array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      this.setState(
+        () => {
+          return { monsters: res.data };
+        },
+        () => {
+          console.log(this.state); //call back function
+        }
+      );
+    });
+  }
+  searchMonster(e) {
+    const filteredMonster = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    this.setState(() => {
+      return { monsters: filteredMonster };
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <input
+          className="input-search"
+          type="search"
+          placeholder="Search Monster"
+          onChange={this.searchMonster.bind(this)}
+        />
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+      </>
+    );
+  }
 }
-
-export default App;
